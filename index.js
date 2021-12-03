babfeb5190176c9062032ddfd113f03ede3ea8e1
Saul Ramirez <fernando.ramire@hotmail.com>
2021-12-03 10:43:53 -0600

    🙈 adding package-lock

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
-const faker = require('faker')
-const { forEach } = require('lodash')
-const myDummyArray = new Array(2000001).fill(0)
-const myOGArray = []
+#!/usr/bin/env node
+const faker = require('faker');
+const { forEach } = require('lodash');
 
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
 
-/** VANILLA JS */
-// console.time("Vanilla")
-// myDummyArray.forEach((_, idx) => {
-//     const name = faker.address.streetName()
-//     const square = (idx + 1) * (idx + 1)
-//     myOGArray.push({ square, name })
-// })
-// console.timeEnd("Vanilla")
- 
-/** LODASH */
-// console.time("lodash")
-// forEach(myDummyArray, (_, idx) => {
-//         const name = faker.address.streetName()
-//         const square = (idx + 1) * (idx + 1)
-//         myOGArray.push({ square, name })
-//     })
-// console.timeEnd("lodash")
+const { argv } = yargs(hideBin(process.argv));
 
-/** VANILLA JS WITH ForLoop */
-console.time("Vanilla For Loop")
-for (let idx = 0; idx < myDummyArray.length; idx++) {
-    const name = faker.address.streetName()
-    const square = (idx + 1) * (idx + 1)
-    myOGArray.push({ square, name })   
+const { size = 2000001, type = '' } = argv;
+
+const myDummyArray = new Array(size).fill(0);
+const myOGArray = [];
+
+const objBuilder = (idx = 0) => {
+  const name = faker.address.streetName();
+  const square = (idx + 1) * (idx + 1);
+  return { square, name };
+};
+
+// eslint-disable-next-line no-console
+console.time(type);
+switch (type.toUpperCase()) {
+  case 'JS':
+    /** VANILLA JS */
+    myDummyArray.forEach((_, idx) => {
+      const obj = objBuilder(idx);
+      myOGArray.push(obj);
+    });
+    break;
+
+  case 'LODASH':
+    /** LODASH */
+    forEach(myDummyArray, (_, idx) => {
+      const obj = objBuilder(idx);
+      myOGArray.push(obj);
+    });
+    break;
+  case 'ForLoop':
+
+    /** VANILLA JS WITH ForLoop */
+    for (let idx = 0; idx < myDummyArray.length; idx += 1) {
+      const obj = objBuilder(idx);
+      myOGArray.push(obj);
+    }
+    break;
+
+  default:
+    // eslint-disable-next-line no-console
+    console.log(`type ${type} no available`);
 }
-console.timeEnd("Vanilla For Loop")
 
+// eslint-disable-next-line no-console
+console.log(`type: ${type} with size: ${size} `);
+// eslint-disable-next-line no-console
+console.timeEnd(type);
